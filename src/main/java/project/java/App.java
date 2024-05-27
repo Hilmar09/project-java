319fd689e9d629c1cb0491eb5f7f70048136136b
Hilmar09 <hil.cris09@gmail.com>
2024-05-27 10:40:09 +0200

    project.java

--- src/main/java/project/java/App.java
+++ src/main/java/project/java/App.java
@@ -1,17 +1,45 @@
 package project.java;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.Scanner;
+
+
+public final class App {
+    public static void main(String[] args)
+    {
+        Scanner sr = new Scanner(System.in);
+        System.out.print("Enter your heigth in meters:");
+        double heigth = sr.nextDouble();
+        System.out.print("Enter your weigth in kilograms:");
+        double weigth = sr.nextDouble();
+        sr.close();
+        double bmi = IdentifieOrNew(heigth, weigth);
+       
+        String classification = "";
+        if(bmi <= 16){
+        classification = "Severe thinness";
+       } else if(bmi <= 17){
+       classification = "Moderate thinnes";
+       } else if(bmi <= 18.5){
+       classification = "Acceptable thinnes";
+       } else if(bmi <= 25){
+        classification = "Normal weigth";
+       } else if(bmi <= 30){
+        classification = "Excess weigth";
+       } else if(bmi <= 35){
+        classification = "Obese: type 1";
+       } else if(bmi <= 40){
+        classification = "Obese: type 2";
+       } else {
+        classification = "Obese; type 3";
+       }
+
+
+       System.out.println("\nYour body mass index is:" + bmi);
+       System.out.println("Attending to the bmi, you have " + classification);
+    }
+    private static double IdentifieOrNew(double heigth, double weigth){
+        double bmi = weigth/(Math.pow(heigth, 2));
+        return Math.rint(bmi*100)/100;
     }
 }
